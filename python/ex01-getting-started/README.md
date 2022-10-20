663afff4dcc4361263e1c5a7c0f399f7036fc869
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2022-10-20 15:16:54 +0200

    cleanup

--- python/ex01-getting-started/README.md
+++
@@ -1,8 +0,0 @@
-# Getting started
-
-This is a first example to show you how to run a basic job. After connecting
-to the cluster, run
-
-```
-./submit.sh
-```
--- scripts/sub_job.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-rm oar_files/*
-
-oarsub -l walltime=24:00:00 -p "host='n03'" --stdout='oar_files/log.txt' --stderr='oar_files/err.txt' --array-param-file param_file.txt "bash launch.sh" 
